f2ad2f56667524e3c0404e0a7ffcffa1e46dcf7d
mennaahmed85 <95880757+mennaahmed85@users.noreply.github.com>
2024-07-21 00:46:54 +0300

    Update Even or Odd.js

--- Even or Odd.js
+++ Even or Odd.js
@@ -1,3 +1,8 @@
+/*
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+
 function isEven(number) {
   //check if the number is even or not 
   if (number % 2 == 0) return "Even";
